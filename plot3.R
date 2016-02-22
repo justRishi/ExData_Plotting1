01782462bb9d1c57e10b8594c1d8f56740896dea
justRishi <justRishi@gmail.com>
2016-02-22 03:22:35 +0100

    added double code in all plot R files

--- plot3.R
+++ plot3.R
@@ -1,3 +1,14 @@
+## Download only needed one time, so commented out download an d unzip after first use
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","consumption.zip")
+unzip("consumption.zip")
+
+##library(data.table) data.table doesn't work with strptime, switched back to data.frame
+#consumption <-fread("household_power_consumption.txt", na.string="?")
+
+
+consumption <-read.csv2("household_power_consumption.txt", na.string="?",colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),dec=".")
+
+
 c<- subset(consumption,Date=="1/2/2007" | Date=="2/2/2007")
 c$Time<-strptime(paste(c$Date,c$Time, sep=" "),format="%d/%m/%Y %H:%M:%S")
 c$Date<-as.Date(c$Date,"%d/%m/%Y")
